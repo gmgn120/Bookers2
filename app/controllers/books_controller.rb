87c8c3828c991a9c97408813cdb9257218ba0ca1
gmgn120 <gmgn120@gmail.com>
2020-12-09 08:37:27 +0000

    [add]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     if @book.save
        flash[:notice] = "successfully"
-    redirect_to books_path
+    redirect_to book_path(@book.id)
     else
     render :new
     end
@@ -20,6 +20,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    
   end
   
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get "home/about" => "homes#about"
   root to: 'homes#top'
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
   resources :users, only: [:show, :edit, :update, :index]
