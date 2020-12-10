2e2eb632ec008ab52eddd577b6fc028572e709ca
gmgn120 <gmgn120@gmail.com>
2020-12-10 07:47:52 +0000

    [add]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,20 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
   
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-       flash[:notice] = "successfully"
-    redirect_to book_path(@book.id)
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
     else
-    render :new
+      @books = Book.all
+       render :index
     end
   end
   
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get "home/about" => "homes#about"
   root to: 'homes#top'
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :books, only: [:create, :index, :show, :destroy, :edit, :update]
   resources :users, only: [:show, :edit, :update, :index]
 end
